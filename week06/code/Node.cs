ae7ae75dc7ac2374ff2ecb3cc5fecfd8bbc3f079
parien2022 <par22002@byui.edu>
2024-05-30 04:28:28 -0300

    BST added

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,8 +12,12 @@ public class Node {
             // Insert to the left
             if (Left is null)
                 Left = new Node(value);
+
             else
                 Left.Insert(value);
+        }
+        else if (value == Data){
+            return;
         }
         else {
             // Insert to the right
@@ -26,11 +30,36 @@ public class Node {
 
     public bool Contains(int value) {
         // TODO Start Problem 2
+        if(value < Data){
+            if (Left is null)
+                return false;
+            else
+                return Left.Contains(value);
+            
+        }
+        if (value == Data){
+            return true;
+        }
+        if(value > Data){
+            if (Right is null)
+                return false;
+            else
+                return Right.Contains(value);
+        }
         return false;
     }
 
     public int GetHeight() {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int countLeft = 0;
+        int countRight = 0;
+
+        if (Left is not null)
+            countLeft = Left.GetHeight();
+
+        if (Right is not null){
+            countRight = Right.GetHeight();
+        }
+        return Math.Max(countLeft, countRight) + 1;
     }
 }
